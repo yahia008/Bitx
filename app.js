5bd96fe5ce7e8e9539b54715db61cc7fc348bbb7
gregemax <gregemax700@gmail.com>
2024-04-24 03:15:06 +0100

    still on it

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-const express=require('express')
+process.on('uncaughtException', (error) => {
+  console.log(error.name, error.message);
+  
+    process.exit(1);
+  
+});
+
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+
+
+dotenv.config({path:'./BitX.env'})
+const app = express();
+mongoose
+  .connect(process.env.connect)
+  .then((err) => console.log("good👍"))
+  .catch((err) => console.log("bad👎")); 
+
+let applis = app.listen(3000, () => {});
+
+process.on("unhandledRejection", (error) => { 
+  console.log(error.name, error.message);
+  applis.close(() => {
+    process.exit(1);
+  });
+});
