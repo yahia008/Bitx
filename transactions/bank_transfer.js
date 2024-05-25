47ed175f07e1543440d17052d09be44934b87f3b
yahia008 <120284117+yahia008@users.noreply.github.com>
2024-05-25 08:51:46 -0700

    Update bank_transfer.js
    github update

--- transactions/bank_transfer.js
+++ transactions/bank_transfer.js
@@ -9,7 +9,7 @@ const flw = new Flutterwave(
 );
 
 const bank_trf = async (req, res) => {
-<<<<<<< HEAD
+
     const {email, amount, phone_number} = req.body
     const tx_ref = generateTxRef()
  try
@@ -35,7 +35,7 @@ const bank_trf = async (req, res) => {
             {
                
                     user_balance.balance += amount
-                    await user_balance.save()
+                    await user_balance.save({validateBeforSave:false})
                     user_tx = new  tx_model({email, amount,  phone_number, type: 'deposit'})
                     await user_tx.save()
             }
@@ -52,45 +52,9 @@ const bank_trf = async (req, res) => {
             console.error('Error initiating bank transfer:', error);
             res.status(500).json({ error: 'Internal Server Error' });
         }
-=======
-  const { email, amount, phone_number } = req.body;
-  const tx_ref = generateTxRef();
-  try {
-   const user_balance = await Authmodel.findOne({ email });
-    console.log(user_balance);
-    if (!user_balance) {
-      return res.status(404).json({ message: "User not found" });
->>>>>>> 7235d2edb21062f8d51aae0dbf0d9afccb70539b
-    }
 
-    const payload = {
-      tx_ref: tx_ref, // tx_ref it a random number
-      amount: amount,
-      email: email,
-      phone_number: phone_number,
-      currency: "NGN",
-    };
-
-    const data = await flw.Charge.bank_transfer(payload); //making the api request
-    if (data.status == "success") {
-      user_balance.balance += amount;
-      await user_balance.save({ validateBeforeSave: false });
-      user_tx = new tx_model({ email, amount, phone_number, type: "deposit" });
-      await user_tx.save({ validateBeforeSave: false });
-    }
-    // then update the ballance
-
-    res.status(200).json(data);
-  } catch (error) {
-    if (error.name === "validationError") {
-      res.status(400).json({ error: error.message });
-    } else {
-      // Handle other errors
-      console.error("Error initiating bank transfer:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  }
 };
+}
 
 const ussd_trf = async (req, res) => {
   const { email, amount, phone_number } = req.body;
