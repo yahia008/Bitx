fa3bac97178d2958cc9d182fd0b4148c53d5d34b
gregemax <140010035+gregemax@users.noreply.github.com>
2024-05-28 12:01:20 +0100

    Update authroute.js

--- routers/authroute.js
+++ routers/authroute.js
@@ -17,17 +17,15 @@ authroute.route("/signup").post(signup);
 authroute.route("/login").post(login);
 authroute.route("/forgotingpassword").post(forgotingpassword);
 authroute.route("/resetpassword/:token").patch(resetpassword);
-<<<<<<< HEAD
 authroute.route("/changepassword").patch(authorize,changepassword);
 authroute.route("/getall").get(authorize,role('admin'),getalluser);
 authroute.route("/getalluser/:id").get(authorize,role('admin'),getalluserbyid);
-=======
 authroute.route("/changepassword").patch(authorize, changepassword);
 authroute.route("/getalluser").get(authorize, role("admin"), getalluser);
 authroute
   .route("/findbyemail")
   .get(authorize, role("admin"), getuserbyemail);
 authroute.route("/getuser/:id").get(authorize, role("admin"), getalluserbyid);
->>>>>>> e6170d844332b394c74421f5d92cfc204fe5edcc
+
 
 module.exports = authroute;
