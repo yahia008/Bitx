3261462d69a16dc33324307f7d09be9ff8e71e3b
gregemax <gregemax700@gmail.com>
2024-06-19 09:27:18 +0100

    first commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const corsOPtion = {
   methods: ["GET", "PATCH", "POST", "DELETE"],
   allowedHeaders: ["content-Type", "Autuorization"],
 };
-app.use(cors(corsOPtion))
+app.use(cors())
 app.use(cp());
 app.use("/auth", authroute);
 app.use("/transaction", tx_route);
