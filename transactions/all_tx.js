fe0ba858282c9a8111f0ddd7b06a6574a3b7269c
gregemax <gregemax700@gmail.com>
2024-06-21 19:38:54 +0100

    tx

--- transactions/all_tx.js
+++ transactions/all_tx.js
@@ -2,12 +2,13 @@ const asynchandle = require("../errors/asynchandler");
 const Transaction = require("../modals/trxmodel");
 
 exports.getalltx = asynchandle(async (req, res, next) => {
-  let user = Transaction.find({ user: req.user.id });
-  user = user.sort("-date");
-  const data = await user; 
+  let emm = req.user.id;
+  let user = await Transaction.find({ user: emm });
+  //user = user.sort("-date");
+  //const data = await user; 
   res.status(200).json({
     message: "success",
-    data,
+    user,
   });
 });
 exports.gettx = asynchandle(async (req, res, next) => {
