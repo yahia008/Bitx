447e70aee4a216fc81a7a9ccc9931f756fca50ff
gregemax <gregemax700@gmail.com>
2024-06-21 15:45:21 +0100

    tx

--- routers/tx_route.js
+++ routers/tx_route.js
@@ -5,13 +5,14 @@ const {
 } = require("../transactions/bank_payment");
 //const { bank_trf, ussd_trf, trx } = require("../transactions/bank_transfer");
 const { withdrawal } = require("../transactions/withdrawal");
-const { getalltx } = require("../transactions/all_tx");
+const { getalltx, gettx } = require("../transactions/all_tx");
 const { authorize } = require("../controllers/authController");
 
 const tx_route = express.Router();
 tx_route.route("/bank_payment").post(bank_payment);
 tx_route.route("/verify").post(verifyPaymentAndUpdateBalance);
 tx_route.route("/getalltx").get(authorize,getalltx);
+tx_route.route("/gettx/:id").get(gettx);
 //tx_route.route("/bank_transfer").post(bank_trf);
 //tx_route.route("/ussd_transfer").post(ussd_trf);
 //tx_route.route("/trx").post(trx);
--- transactions/all_tx.js
+++ transactions/all_tx.js
@@ -2,10 +2,17 @@ const asynchandle = require("../errors/asynchandler");
 const Transaction = require("../modals/trxmodel");
 
 exports.getalltx = asynchandle(async (req, res, next) => {
-    let emm = req.user.id 
-    const user = await Transaction.find({ user: emm });
+  const user = Transaction.find({ user: req.user.id });
+  user = user.sort("date");
   res.status(200).json({
     success: "success",
     user,
+  });
+});
+exports.gettx = asynchandle(async (req, res, next) => {
+  const user = await Transaction.find({ _id: req.params.id });
+  res.status(200).json({
+    message: "success",
+    Transaction: user,
   });
 });
